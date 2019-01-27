707969c413b3e732c797870b7ac9aa8ea685200a
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-27 20:04:17 +0900

    Create 02

---
+++ 02/02Tests/_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _2Tests.swift
+//  02Tests
+//
+//  Created by 築山朋紀 on 2019/01/27.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import XCTest
+@testable import _2
+
+class _2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
