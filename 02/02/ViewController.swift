f9419c8ff865d12bf4d9d3edea1fda1347f912ae
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-27 20:20:00 +0900

    Add UI

--- 02/02/ViewController.swift
+++ 02/02/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
+extension UIButton {
+    open override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1
+    }
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var button1: UIButton!
+    @IBOutlet private weak var button2: UIButton!
+    @IBOutlet private weak var button3: UIButton!
+    @IBOutlet private weak var button4: UIButton!
+    @IBOutlet private weak var button5: UIButton!
+    @IBOutlet private weak var button6: UIButton!
+    @IBOutlet private weak var button7: UIButton!
+    @IBOutlet private weak var button8: UIButton!
+    @IBOutlet private weak var button9: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
 
