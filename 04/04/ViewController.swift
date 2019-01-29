3135d813600536a800b747820469b19517bf7659
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-29 22:20:54 +0900

    Create

--- 04/04/ViewController.swift
+++ 04/04/ViewController.swift
@@ -8,14 +8,70 @@
 
 import UIKit
 
+enum Arithmetic {
+    case plus
+    case minus
+    case multiply
+    case division
+}
+
 class ViewController: UIViewController {
+    
+    private var number1 = 0
+    private var number2 = 0
+    private var arithmetic: Arithmetic = .plus
+    @IBOutlet private weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    @IBAction private func clear() {
+        number1 = 0
+        number1 = 0
+        label.text = "0"
+    }
+    
+    @IBAction private func numberTappend(sender: UIButton) {
+        number1 = number1 * 10 + sender.tag
+        label.text = String(number1)
+    }
+    
+    @IBAction private func calculation(sender: UIButton) {
+        
+        number2 = number1
+        number1 = 0
+        
+        switch sender.tag {
+        case 1:
+            arithmetic = .plus
+        case 2:
+            arithmetic = .minus
+        case 3:
+            arithmetic = .multiply
+        case 4:
+            arithmetic = .division
+        default:
+            break
+        }
+    }
+    
+    @IBAction private func equal() {
+        var num = 0
+        
+        switch arithmetic {
+        case .plus:
+            num = number2 + number1
+        case .minus:
+            num = number2 - number1
+        case .multiply:
+            num = number2 * number1
+        case .division:
+            num = number2 / number1
+        }
+        label.text = String(num)
+    }
 }
 
 extension UIButton {
