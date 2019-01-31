ba08497f8fccbded98d9deba680f017df9680594
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-31 18:16:14 +0900

    Update 現在地の取得

--- 06/06/ViewController.swift
+++ 06/06/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    private var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setupLocationManager()
     }
-
-
+    
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+        
+        guard let locationManager = locationManager else { return }
+        
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        guard let latitude = location?.coordinate.latitude else { return }
+        guard let longitude = location?.coordinate.longitude else { return }
+        
+        print("latitude: \(latitude)\nlongitude: \(longitude)")
+        
+//        https://www.google.com/maps?q=35.6694219,139.4612045
+        print("https://www.google.com/maps?q=\(latitude),\(longitude)")
+    }
 }
 
