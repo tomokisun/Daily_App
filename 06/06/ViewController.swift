94233db79086d1e0fd2fde28f20bece587431f9d
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-31 18:52:42 +0900

    Update Skackに送信

--- 06/06/ViewController.swift
+++ 06/06/ViewController.swift
@@ -13,11 +13,16 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     private var locationManager: CLLocationManager!
-
+    @IBOutlet private weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction private func heyAction() {
         self.setupLocationManager()
+        locationManager.startUpdatingLocation()
     }
     
     func setupLocationManager() {
@@ -41,8 +46,40 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         print("latitude: \(latitude)\nlongitude: \(longitude)")
         
-//        https://www.google.com/maps?q=35.6694219,139.4612045
+      //https://www.google.com/maps?q=35.6694219,139.4612045
         print("https://www.google.com/maps?q=\(latitude),\(longitude)")
+        self.showRequest(location: "https://www.google.com/maps?q=\(latitude),\(longitude)")
+    }
+    
+    private func showRequest(location: String) {
+        let url = ""
+        let text = """
+        呼び出されました。
+        住所: \(location)
+        """
+        
+        let parameters: Parameters = [
+            "channel": "#daily_app",
+            "username": "オカン送り迎え",
+            "text": text
+        ]
+        
+        Alamofire.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).response { response in
+            if response.response?.statusCode == 200 {
+                self.label.text = "オカンを呼びました。\n到着までお待ち下さい。"
+            } else {
+                self.label.text = "error!!\nもう一度タップしてください"
+            }
+        }
+    }
+}
+
+extension UIButton {
+    open override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        self.layer.cornerRadius = rect.width / 2
+        self.clipsToBounds = true
     }
 }
 
