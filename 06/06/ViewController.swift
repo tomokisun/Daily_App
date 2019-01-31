caf93e2e300accf1986a1b269b46ec110f519b2a
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-31 18:18:02 +0900

    Add Alamofire

--- 06/06/ViewController.swift
+++ 06/06/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
---
+++ 06/Pods/Target Support Files/Pods-06/Pods-06-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_06 : NSObject
+@end
+@implementation PodsDummy_Pods_06
+@end
