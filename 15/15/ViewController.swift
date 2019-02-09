cdb09306b3f81c528b19b1967aead364ea1a1763
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-02-09 17:10:40 +0900

    Add

--- 15/15/ViewController.swift
+++ 15/15/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Accounts
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction private func tappend() {
+        let shareImage = UIImage(named: "hanage")!
+        let activityItems = [shareImage]
+        let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        // 使用しないアクティビティタイプ
+        let excludedActivityTypes = [
+            UIActivity.ActivityType.postToFacebook,
+            UIActivity.ActivityType.postToTwitter,
+            UIActivity.ActivityType.message,
+            UIActivity.ActivityType.mail,
+            UIActivity.ActivityType.print,
+            UIActivity.ActivityType.copyToPasteboard,
+            UIActivity.ActivityType.assignToContact,
+            UIActivity.ActivityType.saveToCameraRoll,
+            UIActivity.ActivityType.addToReadingList,
+            UIActivity.ActivityType.saveToCameraRoll,
+            UIActivity.ActivityType.addToReadingList,
+            UIActivity.ActivityType.postToFlickr,
+            UIActivity.ActivityType.postToVimeo,
+            UIActivity.ActivityType.postToWeibo,
+            UIActivity.ActivityType.postToTencentWeibo,
+            UIActivity.ActivityType.openInIBooks,
+            UIActivity.ActivityType.markupAsPDF
+        ]
+        activityVC.excludedActivityTypes = excludedActivityTypes
+        self.present(activityVC, animated: true)
+    }
 }
 
---
+++ 15/README.md
@@ -0,0 +1,12 @@
+# ﾊﾅｹﾞﾃﾞﾃﾙﾖ
+
+リクエスト
+
+<img width="488" alt="image" src="https://user-images.githubusercontent.com/28350464/52518344-d14c6880-2c8c-11e9-8126-80261a818675.png">
+
+
+
+作った
+
+![15](https://user-images.githubusercontent.com/28350464/52518382-33a56900-2c8d-11e9-9394-110097a1a605.gif)
+
