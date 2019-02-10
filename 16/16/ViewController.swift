fbd95475fbdce64f4b76567cb32d6aef9d222c40
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-02-10 19:53:17 +0900

    Update チャットの表示

--- 16/16/ViewController.swift
+++ 16/16/ViewController.swift
@@ -7,14 +7,178 @@
 //
 
 import UIKit
+import MessageKit
+import MessageInputBar
 
-class ViewController: UIViewController {
-
+class ChatViewController: MessagesViewController {
+    
+    private var messageList: [Messages] = []
+    
+    lazy var formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        DispatchQueue.main.async {
+            // messageListにメッセージの配列をいれて
+            self.messageList = self.getMessages()
+            // messagesCollectionViewをリロードして
+            self.messagesCollectionView.reloadData()
+            // 一番下までスクロールする
+            self.messagesCollectionView.scrollToBottom()
+        }
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messagesCollectionView.messageCellDelegate = self
+        
+        messageInputBar.delegate = self
+        messageInputBar.sendButton.tintColor = UIColor.lightGray
+        
+        // メッセージ入力時に一番下までスクロール
+        maintainPositionOnKeyboardFrameChanged = true // default false
+        
+        navigationItem.title = otherSender().displayName
     }
+    
+    // サンプル用に適当なメッセージ
+    private func getMessages() -> [Messages] {
+        return [
+            createmessage(text: "こんちには", sender: otherSender()),
+            createmessage(text: "こちらこそ", sender: currentSender()),
+            createmessage(text: "hogehoge\nhogehoge", sender: otherSender())
+        ]
+    }
+    
+    private func createmessage(text: String, sender: Sender) -> Messages {
+        let attributedText = NSAttributedString(string: text)
+        return Messages(attributedText: attributedText, sender: sender, messageId: UUID().uuidString, date: Date())
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
 
+extension ChatViewController: MessagesDataSource {
+    
+    func currentSender() -> Sender {
+        return Sender(id: "123", displayName: "自分")
+    }
+    
+    func otherSender() -> Sender {
+        return Sender(id: "456", displayName: "知らない人")
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messageList.count
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messageList[indexPath.section]
+    }
+    
+    // メッセージの上に文字を表示
+    func cellTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        if indexPath.section % 3 == 0 {
+            return NSAttributedString(
+                string: MessageKitDateFormatter.shared.string(from: message.sentDate),
+                attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 10),
+                             NSAttributedString.Key.foregroundColor: UIColor.darkGray]
+            )
+        }
+        return nil
+    }
+    
+    // メッセージの上に文字を表示（名前）
+    func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        let name = message.sender.displayName
+        return NSAttributedString(string: name, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption1)])
+    }
+    
+    // メッセージの下に文字を表示（日付）
+    func messageBottomLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        let dateString = formatter.string(from: message.sentDate)
+        return NSAttributedString(string: dateString, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption2)])
+    }
+}
 
+extension ChatViewController: MessagesDisplayDelegate {
+    
+    // メッセージの色を変更（デフォルトは自分：白、相手：黒）
+    func textColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+        return isFromCurrentSender(message: message) ? .white : .darkText
+    }
+    
+    // メッセージの背景色を変更している（デフォルトは自分：緑、相手：グレー）
+    func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+        return isFromCurrentSender(message: message) ?
+            UIColor(red: 69/255, green: 193/255, blue: 89/255, alpha: 1) :
+            UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
+    }
+    
+    // メッセージの枠にしっぽを付ける
+    func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle {
+        let corner: MessageStyle.TailCorner = isFromCurrentSender(message: message) ? .bottomRight : .bottomLeft
+        return .bubbleTail(corner, .curved)
+    }
+    
+    // アイコンをセット
+    func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+        // message.sender.displayNameとかで送信者の名前を取得できるので
+        // そこからイニシャルを生成するとよい
+        let avatar = Avatar(initials: "人")
+        avatarView.set(avatar: avatar)
+    }
+}
+
+extension ChatViewController: MessagesLayoutDelegate {
+    
+    func cellTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        if indexPath.section % 3 == 0 { return 10 }
+        return 0
+    }
+    
+    func messageTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 16
+    }
+    
+    func messageBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 16
+    }
+}
+
+extension ChatViewController: MessageCellDelegate {
+    // メッセージをタップした時の挙動
+    func didTapMessage(in cell: MessageCollectionViewCell) {
+        print("Message tapped")
+    }
+}
+
+extension ChatViewController: MessageInputBarDelegate {
+    // メッセージ送信ボタンをタップした時の挙動
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        for component in inputBar.inputTextView.components {
+            if let image = component as? UIImage {
+                
+                let imageMessage = Messages(image: image, sender: currentSender(), messageId: UUID().uuidString, date: Date())
+                messageList.append(imageMessage)
+                messagesCollectionView.insertSections([messageList.count - 1])
+                
+            } else if let text = component as? String {
+                let attributedText = NSAttributedString(string: text)
+                let message = Messages(attributedText: attributedText, sender: currentSender(), messageId: UUID().uuidString, date: Date())
+                messageList.append(message)
+                messagesCollectionView.insertSections([messageList.count - 1])
+            }
+        }
+        inputBar.inputTextView.text = String()
+        messagesCollectionView.scrollToBottom()
+    }
 }
 
