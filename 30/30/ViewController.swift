633b1e994def784db1be2d95e62fc3ea2a57bf1e
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-02-24 21:52:52 +0900

    Create Day30

---
+++ 30/30/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  30
+//
+//  Created by 築山朋紀 on 2019/02/24.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
