e00b5c56fbe0f944f3a56d0f873ca68ebd6e03b3
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-02-24 22:26:28 +0900

    Update

--- 30/30/ViewController.swift
+++ 30/30/ViewController.swift
@@ -7,14 +7,69 @@
 //
 
 import UIKit
+import Alamofire
+import SafariServices
 
 class ViewController: UIViewController {
-
+    
+    private let cellID = "articleCell"
+    private var articles = [ArticleModel]()
+    
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        send()
     }
-
-
+    
+    private func send() {
+        Alamofire.request("https://qiita.com/api/v2/items?page=1&per_page=20").responseJSON { response in
+            guard let data = response.data else { return }
+            let result = try! JSONDecoder().decode([ArticleModel].self, from: data)
+            self.articles += result
+            self.tableView.reloadData()
+        }
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        cell.textLabel?.text = articles[indexPath.row].title
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let safariView = SFSafariViewController(url: articles[indexPath.row].URL)
+        present(safariView, animated: true)
+    }
+}
+
+struct ArticleModel: Codable {
+    /// 投稿のタイトル
+    let title: String
+    
+    /// 投稿のURL
+    let URL: Foundation.URL
+}
+
+extension ArticleModel {
+    
+    enum CodingKeys: String, CodingKey {
+        case title        = "title"
+        case URL          = "url"
+    }
+    
+}
