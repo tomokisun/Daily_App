b35e7b7430711712b8ea390f59a13c8fe3bf5869
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-02-25 20:03:02 +0900

    Update 現在の場所にフォーカスすることは出来た

--- 31/31/ViewController.swift
+++ 31/31/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet private weak var mapView: MKMapView!
+    
+    private let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupLocationManager()
     }
-
-
+    
+    func setupLocationManager() {
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        let location = locations.first
+        guard let latitude = location?.coordinate.latitude else { return }
+        guard let longitude = location?.coordinate.longitude else { return }
+        
+        print("latitude: \(latitude)\nlongitude: \(longitude)")
+        
+        //https://www.google.com/maps?q=35.6694219,139.4612045
+        print("https://www.google.com/maps?q=\(latitude),\(longitude)")
+        
+        let center = CLLocationCoordinate2DMake(latitude, longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 }
 
