0f995d31f87813314c5bee444478e903f4eef5db
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-02-28 20:42:57 +0900

    Add

--- 34/34/ViewController.swift
+++ 34/34/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let lunch = ["天ぷら かねき", "Anchor Point (アンカー ポイント)｜赤坂 ステーキ ディナー 二次会 結婚式/永田町 ランチ", "アジアンビストロ Dai 東京ガーデンテラス店", "蒼天", "中華料理 上海大飯店"]
+    @IBOutlet private weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction private func action() {
+        label.text = lunch[Int.random(in: 0...lunch.count - 1)]
+    }
 
 }
 
