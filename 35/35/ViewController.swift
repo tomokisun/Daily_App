f15cf3357a931a09646ec895efe64d8082b92095
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-01 22:14:41 +0900

    Update

---
+++ 35/35/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  35
+//
+//  Created by 築山朋紀 on 2019/03/01.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction private func action() {
+        guard let url = URL(string: "https://twitter.com/intent/tweet?text=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%E5%AE%8C%E5%85%A8%E3%81%AB%E7%90%86%E8%A7%A3%E3%81%97%E3%81%9F") else { return }
+        let SFVC = SFSafariViewController(url: url)
+        present(SFVC, animated: true)
+    }
+
+}
+
