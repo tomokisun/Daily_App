b4f7c0f2dcc93a3ea37dee11c5007fd4195aebe2
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-05 19:31:49 +0900

    Add App

--- 39/39/ViewController.swift
+++ 39/39/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import FontAwesome_swift
 
 class ViewController: UIViewController {
+    
+    private var images = [FontAwesome]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        images.append(.heart)
+        images.append(.burn)
+        images.append(.theaterMasks)
+        images.append(.star)
+        images.append(.moon)
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self.view)
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        imageView.center = CGPoint(x: location.x, y: location.y)
+        imageView.image = UIImage.fontAwesomeIcon(name: images[Int.random(in: 0...images.count - 1)], style: .solid, textColor: .gray, size: CGSize(width: 30, height: 30))
+        self.view.addSubview(imageView)
+    }
 }
 
