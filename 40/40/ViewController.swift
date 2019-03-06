05749d7541e8ecda1abb51f826b0d394eb1348f0
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-06 20:05:36 +0900

    Create Day40

---
+++ 40/40/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  40
+//
+//  Created by 築山朋紀 on 2019/03/06.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
