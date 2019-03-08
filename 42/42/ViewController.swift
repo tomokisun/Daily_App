9dac40843af3057978ecdcbf4e010537cb357d9e
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-08 20:22:46 +0900

    Add API

--- 42/42/ViewController.swift
+++ 42/42/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    private let locationManager = CLLocationManager()
+    
+    private let urlString = "http://geoapi.heartrails.com/api/json?method=searchByGeoLocation&x=135.0&y=35.0"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setUpLocationManager()
+    }
+    
+    private func setUpLocationManager() {
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
     }
 
+    private func request(x: String, y: String) {
+        PostalAPI.fetch(x: x, y: y) { result in
+            switch result {
+            case .success(let value):
+                print(value)
+            case .failure:
+                print("error")
+            }
+        }
+    }
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        let location = locations.first
+        guard let latitude = location?.coordinate.latitude else { return }
+        guard let longitude = location?.coordinate.longitude else { return }
+        request(x: String(latitude), y: String(longitude))
+    }
 }
 
