84f6b1dfdae525b0f43205a31e89b887c3bfab82
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-12 21:50:19 +0900

    Add

--- 46/46/ViewController.swift
+++ 46/46/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction private func action() {
+        guard let text = textField.text else { return }
+        if text.isEmpty { return }
+        imageView.image = emojiToImage(text: text)
+    }
+    
+    @IBAction private func save() {
+        
+    }
+    
+    private func emojiToImage(text: String, size: CGFloat = 100) -> UIImage {
+        let outputImageSize = CGSize.init(width: size, height: size)
+        let baseSize = text.boundingRect(with: CGSize(width: 2048, height: 2048),
+                                         options: .usesLineFragmentOrigin,
+                                         attributes: [.font: UIFont.systemFont(ofSize: size / 2)], context: nil).size
+        let fontSize = outputImageSize.width / max(baseSize.width, baseSize.height) * (outputImageSize.width / 2)
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let textSize = text.boundingRect(with: CGSize(width: outputImageSize.width, height: outputImageSize.height),
+                                         options: .usesLineFragmentOrigin,
+                                         attributes: [.font: font], context: nil).size
+        
+        let style = NSMutableParagraphStyle()
+        style.alignment = NSTextAlignment.center
+        style.lineBreakMode = NSLineBreakMode.byClipping
+        
+        let attr : [NSAttributedString.Key : Any] = [NSAttributedString.Key.font : font,
+                                                     NSAttributedString.Key.paragraphStyle: style,
+                                                     NSAttributedString.Key.backgroundColor: UIColor.clear ]
+        
+        UIGraphicsBeginImageContextWithOptions(outputImageSize, false, 0)
+        text.draw(in: CGRect(x: (size - textSize.width) / 2,
+                             y: (size - textSize.height) / 2,
+                             width: textSize.width,
+                             height: textSize.height),
+                  withAttributes: attr)
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
 }
 
