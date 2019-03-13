5349912900dc46eaa18bb04134eae774ef042f94
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-13 22:40:45 +0900

    Update

--- 47/47/ViewController.swift
+++ 47/47/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    @IBAction private func action() {
+        guard let text1 = textField1.text else { return }
+        guard let text2 = textField2.text else { return }
+        guard let index1 = Int(text1) else { return }
+        guard let index2 = Int(text2) else { return }
+        label.text = "\(index2 / index1)円"
+    }
 }
 
