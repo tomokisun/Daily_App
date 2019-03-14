0761e5eb3c881765ddc9a8897c239d37e3a4e7bd
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-14 23:31:09 +0900

    Add

--- 48/48/ViewController.swift
+++ 48/48/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var redLabel: UILabel!
+    @IBOutlet private weak var greenLabel: UILabel!
+    @IBOutlet private weak var blueLabel: UILabel!
+    @IBOutlet private weak var baseView: UIView!
+    
+    var red: CGFloat = 1 {
+        didSet {
+            setColor()
+        }
+    }
+    var green: CGFloat = 1 {
+        didSet {
+            setColor()
+        }
+    }
+    var blue: CGFloat = 1 {
+        didSet {
+            setColor()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let u = UIColor.init(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
     }
-
-
+    
+    private func setColor() {
+        baseView.backgroundColor = UIColor.init(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    @IBAction private func redSlide(sender: UISlider) {
+        red = CGFloat(sender.value)
+        redLabel.text = "\(sender.value)/255"
+    }
+    @IBAction private func greenSlide(sender: UISlider) {
+        green = CGFloat(sender.value)
+        greenLabel.text = "\(sender.value)/255"
+    }
+    @IBAction private func blueSlide(sender: UISlider) {
+        blue = CGFloat(sender.value)
+        blueLabel.text = "\(sender.value)/255"
+    }
 }
 
---
+++ 48/README.md
@@ -0,0 +1,6 @@
+# カラースライダー
+
+作った
+
+![48](https://user-images.githubusercontent.com/28350464/54364896-2c6ce300-46b1-11e9-9a15-d3a6b031367e.gif)
+
