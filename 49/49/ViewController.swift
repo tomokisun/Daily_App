2dae26f088365f725606b9ef32d77009cc852a52
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-15 21:31:39 +0900

    Add

--- 49/49/ViewController.swift
+++ 49/49/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var arScnView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let scene = SCNScene()
+        arScnView.scene = scene
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        arScnView.session.run(configuration)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        arScnView.session.pause()
+    }
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        guard let currentFrame = arScnView.session.currentFrame else { return  }
+        
+        let viewWidth  = arScnView.bounds.width
+        let viewHeight = arScnView.bounds.height
+        let imagePlane = SCNPlane(width: viewWidth/6000, height: viewHeight/6000)
+        imagePlane.firstMaterial?.diffuse.contents = arScnView.snapshot()
+        imagePlane.firstMaterial?.lightingModel = .constant
+        
+        let planeNode = SCNNode(geometry: imagePlane)
+        arScnView.scene.rootNode.addChildNode(planeNode)
+        
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -0.1
+        planeNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+    }
+    
 }
-
