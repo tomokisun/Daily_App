ae286434555494772c9493e84390362c8ed5e524
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-22 23:13:44 +0900

    Update

--- 56/56/ViewController.swift
+++ 56/56/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import Alamofire
+import CoreLocation
+
+struct Model: Codable {
+//    let minutely: SummaryModel
+//    let hourly: SummaryModel
+    let daily: SummaryModel
+}
+
+struct SummaryModel: Codable {
+    let summary: String
+}
 
 class ViewController: UIViewController {
+    
+    private let locationManager = CLLocationManager()
+    @IBOutlet private weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setUpLocationManager()
+    }
+    
+    private func setUpLocationManager() {
+        locationManager.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
     }
 
+    private func request(latitudez: Double, longitude: Double) {
+        let url = "https://api.darksky.net/forecast/6cb2004c7f86f5bfc66fc1c7b221ade3/\(latitudez),\(longitude)?lang=ja"
+        Alamofire.request(url, method: .get, encoding: JSONEncoding.default).responseJSON { response in
+            switch response.result {
+            case .success:
+                do {
+                    guard let data = response.data else { return }
+                    let decoder = JSONDecoder()
+                    let decoded = try decoder.decode(Model.self, from: data)
+                    print(decoded)
+                    self.label.text = decoded.daily.summary
+                } catch {
+                    print(response)
+                    print("\(type(of: Model.self))型の変換に失敗しました")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        guard let latitude = location?.coordinate.latitude else { return }
+        guard let longitude = location?.coordinate.longitude else { return }
+        request(latitudez: latitude, longitude: longitude)
+    }
 }
 
---
+++ 56/Pods/Target Support Files/Pods-56/Pods-56-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_56 : NSObject
+@end
+@implementation PodsDummy_Pods_56
+@end
