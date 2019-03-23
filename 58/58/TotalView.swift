d35b1e5c04e364b1f6d2c65c75261fe34ae1efdd
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-24 02:36:49 +0900

    Update

--- 58/58/TotalView.swift
+++ 58/58/TotalView.swift
@@ -12,3 +12,11 @@ class TotalView: UIView {
     @IBOutlet private weak var dayLabel: UILabel!
     @IBOutlet private weak var totalPrice: UILabel!
 }
+
+final class ItemTableViewCell: UITableViewCell {
+    @IBOutlet private weak var iconImageView: iconImageView!
+    @IBOutlet private weak var serviceNameLabel: UILabel!
+    @IBOutlet private weak var planLabel: UILabel!
+    @IBOutlet private weak var updateLabel: UILabel!
+    @IBOutlet private weak var priceLabel: UILabel!
+}
