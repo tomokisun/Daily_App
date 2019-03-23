4f6fea75299c37c575690fd953a90b3cff364645
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-23 23:09:56 +0900

    Add

--- 57/57/ViewController.swift
+++ 57/57/ViewController.swift
@@ -7,14 +7,89 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+struct HomeModel: Codable {
+    let body: String
+    let user: UserModel
+    
+    enum CodingKeys: String, CodingKey {
+        case body = "content"
+        case user = "account"
+    }
+}
+
+struct UserModel: Codable {
+    let name: String
+    let iconURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "display_name"
+        case iconURL = "avatar_static"
+    }
+}
+
+protocol NibInitializable {}
+
+extension NibInitializable where Self: UIView {    
+    static var nib: UINib {
+        return UINib(nibName: String(describing: self), bundle: nil)
+    }
+    
+    static var name: String {
+        return String(describing: self)
+    }
+}
+
+extension UIView: NibInitializable {}
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private var homeModel = [HomeModel]()
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.tableFooterView = UIView()
+        tableView.register(HomeTableViewCell.nib, forCellReuseIdentifier: HomeTableViewCell.name)
+        request()
     }
-
-
+    
+    private func request() {
+        Alamofire.request("https://qiitadon.com/api/v1/timelines/public?local=something").responseJSON { response in
+            switch response.result {
+            case .success:
+                do {
+                    guard let data = response.data else { return }
+                    let decoder = JSONDecoder()
+                    let decoded = try decoder.decode([HomeModel].self, from: data)
+                    self.homeModel = decoded
+                    self.tableView.reloadData()
+                } catch {
+                    print("失敗しました")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.name, for: indexPath) as? HomeTableViewCell else {
+            assertionFailure("error")
+            return UITableViewCell()
+        }
+        cell.update(with: homeModel[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return homeModel.count
+    }
 }
 
---
+++ 57/Pods/Target Support Files/Pods-57/Pods-57-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_57 : NSObject
+@end
+@implementation PodsDummy_Pods_57
+@end
