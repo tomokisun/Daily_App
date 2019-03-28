c9948e6262319f653ce3a033fc88303820e5044c
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-28 19:39:43 +0900

    Update

--- 62/62/ViewController.swift
+++ 62/62/ViewController.swift
@@ -14,7 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
+class TableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.allowsMultipleSelection = false
+    }
+}
+
+extension TableViewController {
+    // セルが選択された時に呼び出される
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at:indexPath)
+        cell?.accessoryType = .checkmark
+//        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // セルの選択が外れた時に呼び出される
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at:indexPath)
+        cell?.accessoryType = .none
+    }
+}
