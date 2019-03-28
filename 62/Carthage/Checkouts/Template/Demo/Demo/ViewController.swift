f49f1e08ae7f3ddbb9640c49f1aac0037ca20d3e
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-28 22:17:46 +0900

    Add

--- 62/Carthage/Checkouts/Template/Demo/Demo/ViewController.swift
+++ 62/Carthage/Checkouts/Template/Demo/Demo/ViewController.swift
@@ -1,15 +1,12 @@
 //
 //  ViewController.swift
-//  62
+//  Demo
 //
-//  Created by 築山朋紀 on 2019/03/28.
+//  Created by 築山朋紀 on 2019/03/23.
 //  Copyright © 2019 tomoki. All rights reserved.
 //
 
 import UIKit
-import RxSwift
-import RxCocoa
-import Template
 
 class ViewController: UIViewController {
 
@@ -17,4 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+
 }
+
