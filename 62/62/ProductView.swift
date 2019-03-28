a189e9bd521ffa2e0c6774da995bab57ff1d0bd0
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-28 22:14:50 +0900

    Update

---
+++ 62/62/ProductView.swift
@@ -0,0 +1,21 @@
+//
+//  ProductView.swift
+//  62
+//
+//  Created by 築山朋紀 on 2019/03/28.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class ProductView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
