f03f8bc43f35ec5fbb16cc50f347e25e74b89e47
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-12 23:21:19 +0900

    Update

--- 77/77/ViewController.swift
+++ 77/77/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var incomeInputField: UITextField!
+    @IBOutlet private weak var foodInputField: UITextField!
+    @IBOutlet private weak var educationInputField: UITextField!
+    @IBOutlet private weak var clothesInputField: UITextField!
+    @IBOutlet private weak var entertainmentInputField: UITextField!
+    @IBOutlet private weak var moneyTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    private func returnMoney() -> Int {
+        var 収入: Int {
+            let string = incomeInputField.text!
+            return Int(string) ?? 0
+        }
+        var 飲食費: Int {
+            let string = foodInputField.text!
+            return Int(string) ?? 0
+        }
+        var 教育費: Int {
+            let string = educationInputField.text!
+            return Int(string) ?? 0
+        }
+        var 衣服費: Int {
+            let string = clothesInputField.text!
+            return Int(string) ?? 0
+        }
+        var 交際費: Int {
+            let string = entertainmentInputField.text!
+            return Int(string) ?? 0
+        }
+        return 収入 - (飲食費 + 教育費 + 衣服費 + 交際費)
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        moneyTextField.text = String(returnMoney())
+        return true
+    }
+}
---
+++ 77/README.md
@@ -0,0 +1,4 @@
+#  
+
+![77](https://user-images.githubusercontent.com/28350464/56044127-7cef6300-5d79-11e9-908a-13426c30ded4.gif)
+
