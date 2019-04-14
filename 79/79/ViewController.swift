eff9cb4b65ae4a507b22386cda49488c3c4f681b
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-14 23:09:13 +0900

    Update

--- 79/79/ViewController.swift
+++ 79/79/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
 class SecondViewController: UIViewController {
     @IBOutlet private weak var titleTextInput: UITextField!
     @IBOutlet private weak var dateTextInput: UITextField!
+    private var datePicker = UIDatePicker()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setLayoutDatePicker()
+    }
     
     @IBAction private func didButtonTappend(_ sender: UIButton) {
         let title = titleTextInput.text!
@@ -28,5 +35,31 @@ class SecondViewController: UIViewController {
         if !title.isEmpty, !date.isEmpty {
             print(title, date)
         }
+    }
+    
+    private func setLayoutDatePicker() {
+        datePicker.datePickerMode = .date
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.locale = .current
+        
+        let minDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-DD"
+        datePicker.minimumDate = minDate
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 35))
+        let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        toolbar.setItems([spacelItem, doneItem], animated: true)
+        
+        dateTextInput.inputView = datePicker
+        dateTextInput.inputAccessoryView = toolbar
+    }
+    
+    @objc func done() {
+        dateTextInput.endEditing(true)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        dateTextInput.text = "\(formatter.string(from: datePicker.date))"
     }
 }
