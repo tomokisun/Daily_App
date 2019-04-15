9c95268fbb2e996e4d3379c7e02f6fd4dac06a7f
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-15 22:12:20 +0900

    Update

--- 80/80/ViewController.swift
+++ 80/80/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 import RxSwift
 import RxKeyboard
 
+class Message {
+    let body: String
+    let senderId: Int
+    
+    init(body: String, senderID: Int) {
+        self.body = body
+        self.senderId = senderID
+    }
+}
+
 class ViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
+    private var messages = [Message]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    private var count = 0
     
     @IBOutlet private weak var inputFieldBottom: NSLayoutConstraint!
     @IBOutlet private weak var textField: UITextField! {
@@ -44,26 +60,42 @@ class ViewController: UIViewController {
     @IBAction private func pushButtonDidTappend(_ sender: UIButton) {
         let text = textField.text!
         if text.isEmpty { return }
+        textField.text?.removeAll()
+        messages.append(Message(body: text, senderID: 0))
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.random()
+        }
+    }
+    
+    private func random() {
+        var text: String
+        if count == 0 {
+            text = "うーんどうしようか、Googleフォームでなにかいいアイデア来てないかなそろそろネタ切れだね、、！"
+        } else {
+            text = "みんなアイデアを投稿して欲しい！！"
+        }
+        messages.append(Message(body: text, senderID: 1))
+        count = 1
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return messages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row % 2 == 0 {
+        if messages[indexPath.row].senderId == 0 {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: HogeTableViewCell.name, for: indexPath) as? HogeTableViewCell else {
                 fatalError("error")
-                return UITableViewCell()
             }
+            cell.bodyLabel.text = messages[indexPath.row].body
             return cell
         } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: FugaTableViewCell.name, for: indexPath) as? FugaTableViewCell else {
                 fatalError("error")
-                return UITableViewCell()
             }
+            cell.bodyLabel.text = messages[indexPath.row].body
             return cell
         }
     }
