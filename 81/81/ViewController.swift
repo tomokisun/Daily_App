a8dd082e69a1c54f09feaf2b186022e38a090a3e
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-16 23:25:48 +0900

    Update

--- 81/81/ViewController.swift
+++ 81/81/ViewController.swift
@@ -9,12 +9,94 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var resetButton: UIButton!
+    @IBOutlet private weak var restartButton: UIButton!
+    @IBOutlet private weak var startButton: UIButton!
+    @IBOutlet private weak var stopButton: UIButton!
+    @IBOutlet private weak var label: UILabel!
+    
+    var count = 432
+    var timer: Timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        state(of: .stopped)
     }
-
-
+    
+    @IBAction func start() {
+        if !timer.isValid {
+            //タイマーが動作していなかったら動かす
+            timer = Timer.scheduledTimer(
+                timeInterval: 1.0,
+                target: self,
+                selector: #selector(self.up),
+                userInfo: nil,
+                repeats: true
+            )
+            state(of: .playing)
+        }
+    }
+    
+    @IBAction func stop() {
+        if timer.isValid {
+            //タイマーが動作していたら停止する
+            timer.invalidate()
+        }
+        state(of: .paused)
+    }
+    
+    @IBAction func reset() {
+        count = 0
+        label.text = "00:00"
+        if timer.isValid {
+            //タイマーが動作していたら停止する
+            timer.invalidate()
+        }
+        state(of: .stopped)
+    }
+    
+    @objc func up() {
+        count += 1
+        label.text = toTime(TimeInterval(count))
+    }
+    
+    func state(of hidden: hidden) {
+        switch hidden {
+        case .stopped:
+            resetButton.isHidden = true
+            stopButton.isHidden = true
+            restartButton.isHidden = true
+            startButton.isHidden = false
+        case .playing:
+            startButton.isHidden = true
+            resetButton.isHidden = true
+            restartButton.isHidden = true
+            stopButton.isHidden = false
+        case .paused:
+            startButton.isHidden = true
+            stopButton.isHidden = true
+            resetButton.isHidden = false
+            restartButton.isHidden = false
+        }
+    }
+    
+    private func toTime(_ b: TimeInterval) -> String? {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .positional
+        formatter.allowedUnits = [.minute, .hour, .second]
+        return formatter.string(from: b)
+    }
 }
 
+extension ViewController {
+    enum hidden {
+        /// 停止中
+        case stopped
+        /// 再生中
+        case playing
+        /// 一時停止中
+        case paused
+    }
+}
---
+++ 81/README.md
@@ -0,0 +1,6 @@
+# ストップウォッチ
+
+ストップウォッチを再開発しました
+
+![81](https://user-images.githubusercontent.com/28350464/56217860-e388bf80-609e-11e9-9b8d-896ff85f90c1.gif)
+
