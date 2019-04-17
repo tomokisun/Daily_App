176572fb21e1298f15912dde7fc60335905df95d
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-17 19:46:21 +0900

    Update

--- 82/82/ViewController.swift
+++ 82/82/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var hogeLabel: Label!
+    @IBOutlet private weak var fugaLabel: Label!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction private func buttonDidTappend(_ sender: UIButton) {
+        let hoge = ["目覚まし時計が", "愛犬", "携帯", "行く気が", "給料が"]
+        let fuga = ["鳴らなかった", "風邪", "壊れた", "ない", "なかった"]
+        hogeLabel.text = hoge.randomElement()
+        fugaLabel.text = fuga.randomElement()
+    }
 }
 
+@IBDesignable
+class Label: UILabel {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
+
+@IBDesignable
+class Button: UIButton {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
---
+++ 82/README.md
@@ -0,0 +1,10 @@
+# 遅刻言い訳メーカー
+
+リクエストが来た
+
+![image](https://user-images.githubusercontent.com/28350464/56282138-5c445600-6149-11e9-93f5-519f39ad804a.png)
+
+![82](https://user-images.githubusercontent.com/28350464/56282164-6f572600-6149-11e9-865f-726a4b5b4af4.gif)
+
+
+
