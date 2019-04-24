1f23ba6cb958415d73d52ac729e321ac5ef94988
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-24 21:04:04 +0900

    Update

--- 89/89/ViewController.swift
+++ 89/89/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var outputLabel: UILabel!
+    @IBOutlet private weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 文字列を置換する
+        var str2 = "すき家の牛丼"
+        var str3 = "吉野家の豚丼"
+        
+        // 文字列が含まれていない場合rangeはnil
+        if let range = str2.range(of: "牛丼") {
+            // 置換する(変数を直接操作する)
+            str2.replaceSubrange(range, with: "ビーフストロガノフ")
+        }
+        
+        if let range = str3.range(of: "牛丼") {
+            str3.replaceSubrange(range, with: "ビーフストロガノフ")
+        }
+        
+        print(str2)    // すき家のビーフストロガノフ
+        print(str3)    // 吉野家の豚丼
     }
-
-
+    
+    @IBAction private func hoge() {
+        guard var text = textField.text else { return }
+        guard let str = label.text  else { return }
+        if let range = text.range(of: str) {
+            text.replaceSubrange(range, with: "令和")
+        }
+        outputLabel.text = text
+    }
 }
 
---
+++ 89/README.md
@@ -0,0 +1,8 @@
+# 元号変換アプリ
+
+\u32FFを令和に変更するアプリを作った
+
+![89](https://user-images.githubusercontent.com/28350464/56657983-5fed5500-66d4-11e9-88aa-df553c2f7cde.gif)
+
+
+
